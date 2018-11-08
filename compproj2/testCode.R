f97464209f03e82288f6fb5606e364ce1d277540
Patrick Wong <wongpatrick98@gmail.com>
2018-11-08 02:34:01 -0800

    im done

--- compproj2/testCode.R
+++ compproj2/testCode.R
@@ -1,46 +1,88 @@
-runif(500, min = 0, max = 5)
-rbinom(500, size = 15, prob = 0.2)
-rexp(500, rate = 5)
-rpois(500, 2)
+# ###I)
+# library(ggplot2)
+# a <- 0
+# b <- 5
+# sample_size <- 500
+# numList <- runif(sample_size, min = a, max = b)
+# ###II)
+# number_trials <- 15
+# p <- 0.2
+# rbinom(sample_size, size = number_trials, prob = p)
+# ###III)
+# lambda <- 5
+# rexp(sample_size, rate = lambda)
+# ###IV)
+# mu <- 2
+# rpois(sample_size, mu)
 
-nReps = 100
-storedData = rep(NA, nReps)
-for (i in 1:nReps){
-  storedData[i] = i
-  print(storedData[i])
-}
-
-n -> 5
-sim <- rpois(n*500, 2)
 
 
+###Algorithm)
 
-lambda <- 0.2
-n <- 5      
-nosim <- 500    
-simulatedData <- matrix(rexp(n*nosim, 0.2), nrow=nosim, ncol=n)
-simMeans <- apply(simulatedData, 1, mean)
-sampleVar <- round(var(simMeans), 3)
-print(sampleVar)
-theoretcalVar <- round((1/lambda)^2/n,3);
-print(theoretcalVar)
-
-lambda <- 0.2
+#Sample size = 500
 sample_size <- 500
-n <- 5
-storedData = rep(n, sample_size)
-for (i in 1:sample_size){
-  storedData[i] =  i
-}
-
-
-sample_size <- 500
-lambda <- 0.2
+#U(0.5)
 a <- 0
 b <- 5
+#Binomial with number of trials = 15, p = 0.2
+number_trials <- 15
+p <- 0.2
+mu <- 2
+#Exp(5)
+lambda <- 5
+
+#1)
 numList <- runif(sample_size, min = a, max = b)
-n <- 5
-simulatedData5 <- matrix(rexp(n*numList, lambda), nrow=sample_size, ncol=n)
-simMeans5 <- apply(simulatedData5, 1, mean)
-sampleMean5 <- round(mean(simMeans5),3);
-print(sampleMean5)
+
+storedData = rep(numList, sample_size)
+for (i in 1:storedData){
+  storedData[i] = i
+  #2)
+  rbinom(sample_size, size = number_trials, prob = p)
+  #3)
+  rexp(sample_size, rate = lambda)
+  #4)
+  rpois(sample_size, mu)
+  #####################################################
+  mean(storedData)
+}
+
+
+# 
+# ###I)
+# ```{r}
+# library(ggplot2)
+# a <- 0
+# b <- 5
+# sample_size <- 500
+# numList <- runif(sample_size, min = a, max = b)
+# ```
+# ###II)
+# ```{r}
+# number_trials <- 15
+# p <- 0.2
+# rbinom(sample_size, size = number_trials, prob = p)
+# ```
+# ###III)
+# ```{r}
+# lambda <- 5
+# rexp(sample_size, rate = lambda)
+# ```
+# ###IV)
+# ```{r}
+# mu <- 2
+# rpois(sample_size, mu)
+# ```
+# ###Algorithm)
+# ```{r}
+# storedData = rep(NA, sample_size)
+# for (i in 1:sample_size){
+#   storedData[i] = i
+#   n <- 5
+#   m <- 50
+#   average = storedData/n
+#   average2 = storedData/m
+#   vector5 <- average
+#   vector50 <- average2
+# }
+# ```
